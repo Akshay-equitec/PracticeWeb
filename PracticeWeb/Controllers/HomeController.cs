da62d78a7466b1f0789b4571ed25eba0dd929649
Akshay Pandit <panditakshay402@gmail.com>
2024-10-07 11:26:35 +0530

    Add project files.

---
+++ PracticeWeb/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public  ActionResult  Caste(string name, int num)
+        {
+            ViewBag.message = name + " how are u?";
+            ViewBag.number = num;
+            return View();
+        }
+
+        
+    }
+}
