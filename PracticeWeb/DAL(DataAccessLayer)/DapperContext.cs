7b590adb61363bfc8a6b9f29876e5bff7dd914b5
Akshay Pandit <panditakshay402@gmail.com>
2024-10-08 11:31:53 +0530

    added data access layer as DapperContext.cs

---
+++ PracticeWeb/DAL(DataAccessLayer)/DapperContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;    
+
+namespace PracticeWeb.DAL_DataAccessLayer_
+{
+    public class DapperContext
+    {
+        public readonly string _ConnectionString;
+
+
+
+        public DapperContext()
+        {
+            _ConnectionString = ConfigurationManager.ConnectionStrings["EmployeeDBConnection"].ConnectionString;
+        }
+
+
+
+        public IDbConnection CreateConnection()
+        {
+
+            return new SqlConnection(_ConnectionString);
+        }
+
+    }
+}
